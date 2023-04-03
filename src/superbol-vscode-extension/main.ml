b0cb5b6f2f255b436f44f82d2155bc50b4898ec8
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2023-04-03 10:33:49 +0200

    Initial commit

---
+++ src/superbol-vscode-extension/main.ml
@@ -0,0 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (c) 2023 OCamlPro SAS                                       *)
+(*                                                                        *)
+(*  All rights reserved.                                                  *)
+(*  This file is distributed under the terms of the GNU Lesser General    *)
+(*  Public License version 2.1, with the special exception on linking     *)
+(*  described in the LICENSE.md file in the root directory.               *)
+(*                                                                        *)
+(*                                                                        *)
+(**************************************************************************)
+
+
+(* If you delete or rename this file, you should add
+   'src/superbol-vscode-extension/main.ml' to the 'skip' field in "drom.toml" *)
+
+let () = Printf.printf "Hello world!\n"
