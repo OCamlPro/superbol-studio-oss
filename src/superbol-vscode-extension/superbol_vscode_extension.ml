03fdaed325044b2d965b500d7db9125899a33ae1
Emilien Lemaire <emilien.lem@icloud.com>
2023-04-05 14:06:50 +0200

    Add lsp launch

--- src/superbol-vscode-extension/superbol_vscode_extension.ml
+++ src/superbol-vscode-extension/superbol_vscode_extension.ml
@@ -14,10 +14,37 @@
 (* If you delete or rename this file, you should add
    'src/superbol-vscode-extension/main.ml' to the 'skip' field in "drom.toml" *)
 
+let client = ref None
+
 let activate (_extension: Vscode.ExtensionContext.t) =
-  let str = Interop.Js.String.t_to_js "Hello from OCaml" in
-  Js_of_ocaml.Firebug.console##log str;
-  Promise.return ()
+  let oc = Vscode.Window.createOutputChannel ~name:"superbol-vscode-extension" in
+  Vscode.OutputChannel.appendLine oc ~value:"Hello from ocaml";
+  let command = "/home/emilien/development/ocamlpro/padbol/superbol" in
+  let args = ["x-lsp"] in
+  let serverOptions = Vscode_languageclient.ServerOptions.create
+                        ~command
+                        ~args
+                        ()
+  in
+  let documentSelector =
+    [| `Filter (Vscode_languageclient.DocumentFilter.createLanguage ~language:"cobol" ()) |]
+  in
+  let clientOptions = Vscode_languageclient.ClientOptions.create ~documentSelector () in
+  client :=
+    Some (Vscode_languageclient.LanguageClient.make
+            ~id:"cobolServer"
+            ~name:"Cobol Server"
+            ~serverOptions
+            ~clientOptions
+            ());
+  match !client with
+  | Some client -> Vscode_languageclient.LanguageClient.start client
+  | None -> Promise.return ()
+
+let deactivate () =
+  match !client with
+  | None -> Promise.return ()
+  | Some client -> Vscode_languageclient.LanguageClient.stop client
 
 let () =
   Js_of_ocaml.Js.(export "activate" (wrap_callback activate))
