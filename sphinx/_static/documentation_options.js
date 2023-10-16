c56c6e2e55e8e8ca76f746a331a73c3f6e1c8da8
nberth <nberth@member.fsf.org>
2023-10-16 15:04:32 +0000

    Deploying to gh-pages from  @ 9473ea86faf1e1c85b355f836357dc7b096e1f93 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (aeed2c6) (2023/10/16 14:11)',
+    VERSION: 'master (9473ea8) (2023/10/16 15:04)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
