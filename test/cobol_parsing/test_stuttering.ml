OCamlPro/superbol-studio-oss

commit 86e6cca68327776bc9035785bdf91898d6959079

Emit `comment` semantic tokens for SNA and text in the right margin

src/lsp/cobol_preproc/preproc_engine.ml
@@ -47,11 +47,33 @@ and preprocessor_persist =
 let diags { diags; reader; _ } = DIAGS.Set.union diags @@ Src_reader.diags reader
 let add_diag lp d = { lp with diags = DIAGS.Set.cons d lp.diags }
 let add_diags lp d = { lp with diags = DIAGS.Set.union d lp.diags }
-let log { pplog; _ } = pplog
-let source_format { reader; _ } = Src_reader.source_format reader
 let position { reader; _ } = Src_reader.position reader
-let comments { reader; _ } = Src_reader.comments reader
-let newline_cnums { reader; _ } = Src_reader.newline_cnums reader
+let source_format { reader; _ } = Src_reader.source_format reader
+let rev_log { pplog; _ } = pplog
+let rev_comments { reader; _ } = Src_reader.rev_comments reader
+let rev_ignored { reader; _ } = Src_reader.rev_ignored reader
+
+(** [position_at ~line ~char pp] computes a lexing position that corresponds to
+    the given line and character indexes (all starting at 0) in the input
+    already read by [pp].  Raises {!Not_found} if no complete line was processed
+    yet, or the current position if the given line index doed not correspond to
+    already processed lines. *)
+let position_at ~line ~char { reader; _ } =
+  let rev_newline_cnums = Src_reader.rev_newline_cnums reader in
+  if rev_newline_cnums = []
+  then raise Not_found
+  else
+    let lexpos = Src_reader.position reader in
+    try
+      let pos_bol =
+        try List.nth rev_newline_cnums (lexpos.pos_lnum - line - 1)
+        with Not_found | Invalid_argument _ -> 0
+      in
+      Lexing.{ lexpos with pos_bol;
+                           pos_cnum = pos_bol + char;
+                           pos_lnum = line + 1 }
+    with Failure _ ->
+      lexpos
 
 let with_reader lp reader =
   if lp.reader == reader then lp else { lp with reader }

src/lsp/cobol_lsp/lsp_document.ml
@@ -63,6 +63,7 @@ module TYPES = struct
       doc_cache_pplog: Cobol_preproc.Trace.log;
       doc_cache_tokens: Cobol_parser.Outputs.tokens_with_locs;
       doc_cache_comments: Cobol_preproc.Text.comments;
+      doc_cache_ignored: Cobol_common.Srcloc.lexloc list;
       doc_cache_parsed: (Cobol_ptree.compilation_group * CUs.t) option;
       doc_cache_diags: DIAGS.Set.serializable;
     }
@@ -116,7 +117,8 @@ let lazy_references ptree cus defs =
 let no_artifacts =
   Cobol_parser.Outputs.{ tokens = lazy [];
                          pplog = Cobol_preproc.Trace.empty;
-                         comments = [] }
+                         rev_comments = [];
+                         rev_ignored = [] }
 
 let gather_parsed_data ptree =
   Cobol_typeck.analyze_compilation_group ptree |>
@@ -216,7 +218,8 @@ let retrieve_parsed_data: document -> parsed_data = function
 (** Caching utilities *)
 
 let to_cache ({ project; textdoc; parsed; diags;
-                artifacts = { pplog; tokens; comments; _ }; _ } as doc) =
+                artifacts = { pplog; tokens;
+                              rev_comments; rev_ignored; _ }; _ } as doc) =
   {
     doc_cache_filename = Lsp_project.relative_path_for ~uri:(uri doc) project;
     doc_cache_checksum = Digest.string (Lsp.Text_document.text textdoc);
@@ -224,7 +227,8 @@ let to_cache ({ project; textdoc; parsed; diags;
     doc_cache_version = Lsp.Text_document.version textdoc;
     doc_cache_pplog = pplog;
     doc_cache_tokens = Lazy.force tokens;
-    doc_cache_comments = comments;
+    doc_cache_comments = rev_comments;
+    doc_cache_ignored = rev_ignored;
     doc_cache_parsed = Option.map (fun { ptree; cus; _ } -> ptree, cus) parsed;
     doc_cache_diags = DIAGS.Set.apply_delayed_formatting diags;
   }
@@ -239,7 +243,8 @@ let of_cache ~project
       doc_cache_version = version;
       doc_cache_pplog = pplog;
       doc_cache_tokens = tokens;
-      doc_cache_comments = comments;
+      doc_cache_comments = rev_comments;
+      doc_cache_ignored = rev_ignored;
       doc_cache_parsed = parsed;
       doc_cache_diags = diags } =
   let absolute_filename = Lsp_project.absolute_path_for ~filename project in
@@ -260,7 +265,8 @@ let of_cache ~project
            { ptree; cus; definitions; references })
         parsed
     in
-    { doc with artifacts = { pplog; tokens = lazy tokens; comments };
+    { doc with artifacts = { pplog; tokens = lazy tokens;
+                             rev_comments; rev_ignored };
                diags = DIAGS.Set.of_serializable diags;
                parsed }
 

src/lsp/cobol_lsp/lsp_request.ml
@@ -216,11 +216,13 @@ let handle_semtoks_full,
     handle_semtoks_range =
   let handle registry ?range (doc: TextDocumentIdentifier.t) =
     try_with_document_data registry doc
-      ~f:begin fun ~doc:{ artifacts = { pplog; tokens; comments; _ };
+      ~f:begin fun ~doc:{ artifacts = { pplog; tokens;
+                                        rev_comments; rev_ignored; _ };
                           _ } Lsp_document.{ ptree; _ } ->
         let data =
           Lsp_semtoks.data ~filename:(Lsp.Uri.to_path doc.uri) ~range
-            ~pplog ~comments ~tokens:(Lazy.force tokens) ~ptree
+            ~pplog ~rev_comments ~rev_ignored
+            ~tokens:(Lazy.force tokens) ~ptree
         in
         Some (SemanticTokens.create ~data ())
       end

src/lsp/cobol_lsp/lsp_semtoks.ml
@@ -553,17 +553,32 @@ let semtoks_from_ptree ~filename ?range ptree =
 
   end) ptree [] |> List.rev
 
-let semtoks_of_comments ~filename ?range comments = comments |>
-  List.filter_map begin function
+let semtoks_of_comments ~filename ?range rev_comments =
+  rev_comments |>
+  List.fold_left begin fun acc -> function
     | Cobol_preproc.Text.{ comment_loc = s, _ as lexloc; _ }
       when s.Lexing.pos_fname = filename &&
            Option.fold range
              ~some:(fun r -> Lsp_position.intersects_lexloc r lexloc)
              ~none:true ->
-        Some (semtok TOKTYP.comment lexloc)
+        semtok TOKTYP.comment lexloc :: acc
     | _ ->
-        None
-  end
+        acc
+  end []
+
+let semtoks_of_ignored ~filename ?range rev_ignored =
+  (* Decorate like comments, for lack of a better suited token type in the set
+     of available ones.  This could be improved later with some client-side code
+     or configuration.  *)
+  rev_ignored |>
+  List.fold_left begin fun acc ((s, _ ) as lexloc) ->
+    if s.Lexing.pos_fname = filename &&
+       Option.fold range
+         ~some:(fun r -> Lsp_position.intersects_lexloc r lexloc)
+         ~none:true
+    then semtok TOKTYP.comment lexloc :: acc
+    else acc
+  end []
 
 let semtoks_of_preproc_statements ~filename ?range pplog =
   List.rev @@ List.fold_left begin fun acc -> function
@@ -646,11 +661,13 @@ let ensure_sorted name ~filename cmp l =
       List.fast_sort cmp l
 
 
-let data ~filename ~range ~tokens ~pplog ~comments ~ptree : int array =
+let data ~filename ~range ~tokens ~pplog
+    ~rev_comments ~rev_ignored ~ptree : int array =
   let semtoks1 = semtoks_of_non_ambigious_tokens ~filename ?range tokens in
   let semtoks2 = semtoks_from_ptree              ~filename ?range ptree in
-  let semtoks3 = semtoks_of_comments             ~filename ?range comments in
-  let semtoks4 = semtoks_of_preproc_statements   ~filename ?range pplog in
+  let semtoks3 = semtoks_of_comments             ~filename ?range rev_comments in
+  let semtoks4 = semtoks_of_ignored              ~filename ?range rev_ignored in
+  let semtoks5 = semtoks_of_preproc_statements   ~filename ?range pplog in
   (* NB: In *principle* all those lists are already sorted w.r.t lexical
      locations in [filename].  We just check that for now and raise a warning,
      in case. *)
@@ -660,8 +677,10 @@ let data ~filename ~range ~tokens ~pplog ~comments ~ptree : int array =
   let semtoks1 = ensure_sorted "nonambiguous" ~filename compare_semtoks semtoks1
   and semtoks2 = ensure_sorted "ptree"        ~filename compare_semtoks semtoks2
   and semtoks3 = ensure_sorted "comments"     ~filename compare_semtoks semtoks3
-  and semtoks4 = ensure_sorted "preproc"      ~filename compare_semtoks semtoks4 in
+  and semtoks4 = ensure_sorted "ignored"      ~filename compare_semtoks semtoks4
+  and semtoks5 = ensure_sorted "preproc"      ~filename compare_semtoks semtoks5 in
   relative_semtoks
     List.(merge compare_semtoks semtoks1 @@
           merge compare_semtoks semtoks2 @@
-          merge compare_semtoks semtoks3 @@ semtoks4)
+          merge compare_semtoks semtoks3 @@
+          merge compare_semtoks semtoks4 @@ semtoks5)

src/lsp/cobol_lsp/lsp_semtoks.mli
@@ -21,6 +21,7 @@ val data
   -> range: Lsp.Types.Range.t option
   -> tokens: Cobol_parser.Outputs.tokens_with_locs
   -> pplog: Cobol_preproc.Trace.log
-  -> comments: Cobol_preproc.Text.comments
+  -> rev_comments: Cobol_preproc.Text.comments
+  -> rev_ignored: Cobol_common.Srcloc.lexloc list
   -> ptree: Cobol_ptree.compilation_group
   -> int array

commit 227b3689b20e46fe079e56a5018cde06c031ff41

Fix wrong position lookup upon rewind to the first line of input
This fixes a bug introduced in 86e6cca6 by an uncareful optimization
of the computation.  Essentially, the meaning of `Failure` and
`Invalid_argument` exception in `List.nth` needed to be reversed.  The
included changes fix and improve the affected portion of code, and add
some new (high-level) tests to better exercise the rewinding code.

src/lsp/cobol_preproc/preproc_engine.ml
@@ -55,25 +55,27 @@ let rev_ignored { reader; _ } = Src_reader.rev_ignored reader
 
 (** [position_at ~line ~char pp] computes a lexing position that corresponds to
     the given line and character indexes (all starting at 0) in the input
-    already read by [pp].  Raises {!Not_found} if no complete line was processed
-    yet, or the current position if the given line index doed not correspond to
-    already processed lines. *)
+    already read by [pp].  Raises [Not_found] if no complete line was processed
+    yet, or the current position if the given line index does not correspond to
+    an already processed line. *)
 let position_at ~line ~char { reader; _ } =
   let rev_newline_cnums = Src_reader.rev_newline_cnums reader in
-  if rev_newline_cnums = []
+  if rev_newline_cnums = []                                (* no newline seen *)
   then raise Not_found
-  else
-    let lexpos = Src_reader.position reader in
-    try
+  else     (* |rev_newline_cnums| is the number of newline chars seen upto... *)
+    let lexpos = Src_reader.position reader in       (* ... current position. *)
+    if line <= lexpos.pos_lnum - 1                   (* = |rev_newline_cnums| *)
+    then
       let pos_bol =
-        try List.nth rev_newline_cnums (lexpos.pos_lnum - line - 1)
-        with Not_found | Invalid_argument _ -> 0
+        if line <= 0
+        then 0
+        else List.nth rev_newline_cnums (lexpos.pos_lnum - 1 - line)
       in
-      Lexing.{ lexpos with pos_bol;
-                           pos_cnum = pos_bol + char;
-                           pos_lnum = line + 1 }
-    with Failure _ ->
-      lexpos
+      { lexpos with pos_bol;
+                    pos_cnum = pos_bol + char;
+                    pos_lnum = line + 1 }
+    else
+      lexpos                                           (* (line not seen yet) *)
 
 let with_reader lp reader =
   if lp.reader == reader then lp else { lp with reader }

test/cobol_parsing/test_stuttering.ml
@@ -445,3 +445,909 @@ let%expect_test "line-by-line-incremental-small-4" =
       PROCEDURE DIVISION.
         STOP RUN.
 |}];;
+
+let%expect_test "chunk-by-chunk-incremental-small-5" =
+  Parser_testing.iteratively_append_chunks_stuttering ~f:show_ptree @@
+  Parser_testing.extract_position_markers @@
+  Parser_testing.insert_periodic_position_markers ~period:1
+    "   \n\
+    \ \n\
+    \\tIDENTIFICATION DIVISION.\n\
+    \       PROGRAM-ID. prog.\n\
+    \       PROCEDURE\tDIVISION.\n\
+    \          STOP RUN.";
+  [%expect{|
+    Appending chunk 1/103 @ 0:0-0:2 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 1/103 back @ 0:0-0:1 (" ")
+    Parse-tree:
+
+
+    Appending chunk 2/103 @ 0:1-0:3 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 2/103 back @ 0:1-0:2 (" ")
+    Parse-tree:
+
+
+    Appending chunk 3/103 @ 0:2-1:0 (" \n")
+    Parse-tree:
+
+
+    Cutting chunk 3/103 back @ 0:2-0:3 (" ")
+    Parse-tree:
+
+
+    Appending chunk 4/103 @ 0:3-1:1 ("\n ")
+    Parse-tree:
+
+
+    Cutting chunk 4/103 back @ 0:3-1:0 ("\n")
+    Parse-tree:
+
+
+    Appending chunk 5/103 @ 1:0-2:0 (" \n")
+    Parse-tree:
+
+
+    Cutting chunk 5/103 back @ 1:0-1:1 (" ")
+    Parse-tree:
+
+
+    Appending chunk 6/103 @ 1:1-2:1 ("\n\\")
+    Parse-tree:
+
+
+    Cutting chunk 6/103 back @ 1:1-2:0 ("\n")
+    Parse-tree:
+
+
+    Appending chunk 7/103 @ 2:0-2:2 ("\\t")
+    Parse-tree:
+
+
+    Cutting chunk 7/103 back @ 2:0-2:1 ("\\")
+    Parse-tree:
+
+
+    Appending chunk 8/103 @ 2:1-2:3 ("tI")
+    Parse-tree:
+
+
+    Cutting chunk 8/103 back @ 2:1-2:2 ("t")
+    Parse-tree:
+
+
+    Appending chunk 9/103 @ 2:2-2:4 ("ID")
+    Parse-tree:
+
+
+    Cutting chunk 9/103 back @ 2:2-2:3 ("I")
+    Parse-tree:
+
+
+    Appending chunk 10/103 @ 2:3-2:5 ("DE")
+    Parse-tree:
+
+
+    Cutting chunk 10/103 back @ 2:3-2:4 ("D")
+    Parse-tree:
+
+
+    Appending chunk 11/103 @ 2:4-2:6 ("EN")
+    Parse-tree:
+
+
+    Cutting chunk 11/103 back @ 2:4-2:5 ("E")
+    Parse-tree:
+
+
+    Appending chunk 12/103 @ 2:5-2:7 ("NT")
+    Parse-tree:
+
+
+    Cutting chunk 12/103 back @ 2:5-2:6 ("N")
+    Parse-tree:
+
+
+    Appending chunk 13/103 @ 2:6-2:8 ("TI")
+    Parse-tree:
+      None
+
+    Cutting chunk 13/103 back @ 2:6-2:7 ("T")
+    Parse-tree:
+
+
+    Appending chunk 14/103 @ 2:7-2:9 ("IF")
+    Parse-tree:
+      None
+
+    Cutting chunk 14/103 back @ 2:7-2:8 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 15/103 @ 2:8-2:10 ("FI")
+    Parse-tree:
+      None
+
+    Cutting chunk 15/103 back @ 2:8-2:9 ("F")
+    Parse-tree:
+      None
+
+    Appending chunk 16/103 @ 2:9-2:11 ("IC")
+    Parse-tree:
+      None
+
+    Cutting chunk 16/103 back @ 2:9-2:10 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 17/103 @ 2:10-2:12 ("CA")
+    Parse-tree:
+      None
+
+    Cutting chunk 17/103 back @ 2:10-2:11 ("C")
+    Parse-tree:
+      None
+
+    Appending chunk 18/103 @ 2:11-2:13 ("AT")
+    Parse-tree:
+      None
+
+    Cutting chunk 18/103 back @ 2:11-2:12 ("A")
+    Parse-tree:
+      None
+
+    Appending chunk 19/103 @ 2:12-2:14 ("TI")
+    Parse-tree:
+      None
+
+    Cutting chunk 19/103 back @ 2:12-2:13 ("T")
+    Parse-tree:
+      None
+
+    Appending chunk 20/103 @ 2:13-2:15 ("IO")
+    Parse-tree:
+      None
+
+    Cutting chunk 20/103 back @ 2:13-2:14 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 21/103 @ 2:14-2:16 ("ON")
+    Parse-tree:
+      None
+
+    Cutting chunk 21/103 back @ 2:14-2:15 ("O")
+    Parse-tree:
+      None
+
+    Appending chunk 22/103 @ 2:15-2:17 ("N ")
+    Parse-tree:
+      None
+
+    Cutting chunk 22/103 back @ 2:15-2:16 ("N")
+    Parse-tree:
+      None
+
+    Appending chunk 23/103 @ 2:16-2:18 (" D")
+    Parse-tree:
+      None
+
+    Cutting chunk 23/103 back @ 2:16-2:17 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 24/103 @ 2:17-2:19 ("DI")
+    Parse-tree:
+      None
+
+    Cutting chunk 24/103 back @ 2:17-2:18 ("D")
+    Parse-tree:
+      None
+
+    Appending chunk 25/103 @ 2:18-2:20 ("IV")
+    Parse-tree:
+      None
+
+    Cutting chunk 25/103 back @ 2:18-2:19 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 26/103 @ 2:19-2:21 ("VI")
+    Parse-tree:
+      None
+
+    Cutting chunk 26/103 back @ 2:19-2:20 ("V")
+    Parse-tree:
+      None
+
+    Appending chunk 27/103 @ 2:20-2:22 ("IS")
+    Parse-tree:
+      None
+
+    Cutting chunk 27/103 back @ 2:20-2:21 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 28/103 @ 2:21-2:23 ("SI")
+    Parse-tree:
+      None
+
+    Cutting chunk 28/103 back @ 2:21-2:22 ("S")
+    Parse-tree:
+      None
+
+    Appending chunk 29/103 @ 2:22-2:24 ("IO")
+    Parse-tree:
+      None
+
+    Cutting chunk 29/103 back @ 2:22-2:23 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 30/103 @ 2:23-2:25 ("ON")
+    Parse-tree:
+      None
+
+    Cutting chunk 30/103 back @ 2:23-2:24 ("O")
+    Parse-tree:
+      None
+
+    Appending chunk 31/103 @ 2:24-2:26 ("N.")
+    Parse-tree:
+      None
+
+    Cutting chunk 31/103 back @ 2:24-2:25 ("N")
+    Parse-tree:
+      None
+
+    Appending chunk 32/103 @ 2:25-3:0 (".\n")
+    Parse-tree:
+      None
+
+    Cutting chunk 32/103 back @ 2:25-2:26 (".")
+    Parse-tree:
+      None
+
+    Appending chunk 33/103 @ 2:26-3:1 ("\n ")
+    Parse-tree:
+      None
+
+    Cutting chunk 33/103 back @ 2:26-3:0 ("\n")
+    Parse-tree:
+
+
+    Appending chunk 34/103 @ 3:0-3:2 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 34/103 back @ 3:0-3:1 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 35/103 @ 3:1-3:3 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 35/103 back @ 3:1-3:2 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 36/103 @ 3:2-3:4 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 36/103 back @ 3:2-3:3 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 37/103 @ 3:3-3:5 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 37/103 back @ 3:3-3:4 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 38/103 @ 3:4-3:6 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 38/103 back @ 3:4-3:5 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 39/103 @ 3:5-3:7 ("  ")
+    Parse-tree:
+
+
+    Cutting chunk 39/103 back @ 3:5-3:6 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 40/103 @ 3:6-3:8 (" P")
+    Parse-tree:
+      None
+
+    Cutting chunk 40/103 back @ 3:6-3:7 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 41/103 @ 3:7-3:9 ("PR")
+    Parse-tree:
+      None
+
+    Cutting chunk 41/103 back @ 3:7-3:8 ("P")
+    Parse-tree:
+      None
+
+    Appending chunk 42/103 @ 3:8-3:10 ("RO")
+    Parse-tree:
+      None
+
+    Cutting chunk 42/103 back @ 3:8-3:9 ("R")
+    Parse-tree:
+      None
+
+    Appending chunk 43/103 @ 3:9-3:11 ("OG")
+    Parse-tree:
+      None
+
+    Cutting chunk 43/103 back @ 3:9-3:10 ("O")
+    Parse-tree:
+      None
+
+    Appending chunk 44/103 @ 3:10-3:12 ("GR")
+    Parse-tree:
+      None
+
+    Cutting chunk 44/103 back @ 3:10-3:11 ("G")
+    Parse-tree:
+      None
+
+    Appending chunk 45/103 @ 3:11-3:13 ("RA")
+    Parse-tree:
+      None
+
+    Cutting chunk 45/103 back @ 3:11-3:12 ("R")
+    Parse-tree:
+      None
+
+    Appending chunk 46/103 @ 3:12-3:14 ("AM")
+    Parse-tree:
+      None
+
+    Cutting chunk 46/103 back @ 3:12-3:13 ("A")
+    Parse-tree:
+      None
+
+    Appending chunk 47/103 @ 3:13-3:15 ("M-")
+    Parse-tree:
+      None
+
+    Cutting chunk 47/103 back @ 3:13-3:14 ("M")
+    Parse-tree:
+      None
+
+    Appending chunk 48/103 @ 3:14-3:16 ("-I")
+    Parse-tree:
+      None
+
+    Cutting chunk 48/103 back @ 3:14-3:15 ("-")
+    Parse-tree:
+      None
+
+    Appending chunk 49/103 @ 3:15-3:17 ("ID")
+    Parse-tree:
+      None
+
+    Cutting chunk 49/103 back @ 3:15-3:16 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 50/103 @ 3:16-3:18 ("D.")
+    Parse-tree:
+      None
+
+    Cutting chunk 50/103 back @ 3:16-3:17 ("D")
+    Parse-tree:
+      None
+
+    Appending chunk 51/103 @ 3:17-3:19 (". ")
+    Parse-tree:
+      None
+
+    Cutting chunk 51/103 back @ 3:17-3:18 (".")
+    Parse-tree:
+      None
+
+    Appending chunk 52/103 @ 3:18-3:20 (" p")
+    Parse-tree:
+      None
+
+    Cutting chunk 52/103 back @ 3:18-3:19 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 53/103 @ 3:19-3:21 ("pr")
+    Parse-tree:
+      None
+
+    Cutting chunk 53/103 back @ 3:19-3:20 ("p")
+    Parse-tree:
+      None
+
+    Appending chunk 54/103 @ 3:20-3:22 ("ro")
+    Parse-tree:
+      None
+
+    Cutting chunk 54/103 back @ 3:20-3:21 ("r")
+    Parse-tree:
+      None
+
+    Appending chunk 55/103 @ 3:21-3:23 ("og")
+    Parse-tree:
+      None
+
+    Cutting chunk 55/103 back @ 3:21-3:22 ("o")
+    Parse-tree:
+      None
+
+    Appending chunk 56/103 @ 3:22-3:24 ("g.")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Cutting chunk 56/103 back @ 3:22-3:23 ("g")
+    Parse-tree:
+      None
+
+    Appending chunk 57/103 @ 3:23-4:0 (".\n")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Cutting chunk 57/103 back @ 3:23-3:24 (".")
+    Parse-tree:
+      None
+
+    Appending chunk 58/103 @ 3:24-4:1 ("\n ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Cutting chunk 58/103 back @ 3:24-4:0 ("\n")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Appending chunk 59/103 @ 4:0-4:2 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Cutting chunk 59/103 back @ 4:0-4:1 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 60/103 @ 4:1-4:3 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Cutting chunk 60/103 back @ 4:1-4:2 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Appending chunk 61/103 @ 4:2-4:4 ("  ")
+    Parse-tree:
+      None
+
+    Cutting chunk 61/103 back @ 4:2-4:3 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Appending chunk 62/103 @ 4:3-4:5 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Cutting chunk 62/103 back @ 4:3-4:4 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 63/103 @ 4:4-4:6 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Cutting chunk 63/103 back @ 4:4-4:5 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Appending chunk 64/103 @ 4:5-4:7 ("  ")
+    Parse-tree:
+      None
+
+    Cutting chunk 64/103 back @ 4:5-4:6 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+
+    Appending chunk 65/103 @ 4:6-4:8 (" P")
+    Parse-tree:
+      None
+
+    Cutting chunk 65/103 back @ 4:6-4:7 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 66/103 @ 4:7-4:9 ("PR")
+    Parse-tree:
+      None
+
+    Cutting chunk 66/103 back @ 4:7-4:8 ("P")
+    Parse-tree:
+      None
+
+    Appending chunk 67/103 @ 4:8-4:10 ("RO")
+    Parse-tree:
+      None
+
+    Cutting chunk 67/103 back @ 4:8-4:9 ("R")
+    Parse-tree:
+      None
+
+    Appending chunk 68/103 @ 4:9-4:11 ("OC")
+    Parse-tree:
+      None
+
+    Cutting chunk 68/103 back @ 4:9-4:10 ("O")
+    Parse-tree:
+      None
+
+    Appending chunk 69/103 @ 4:10-4:12 ("CE")
+    Parse-tree:
+      None
+
+    Cutting chunk 69/103 back @ 4:10-4:11 ("C")
+    Parse-tree:
+      None
+
+    Appending chunk 70/103 @ 4:11-4:13 ("ED")
+    Parse-tree:
+      None
+
+    Cutting chunk 70/103 back @ 4:11-4:12 ("E")
+    Parse-tree:
+      None
+
+    Appending chunk 71/103 @ 4:12-4:14 ("DU")
+    Parse-tree:
+      None
+
+    Cutting chunk 71/103 back @ 4:12-4:13 ("D")
+    Parse-tree:
+      None
+
+    Appending chunk 72/103 @ 4:13-4:15 ("UR")
+    Parse-tree:
+      None
+
+    Cutting chunk 72/103 back @ 4:13-4:14 ("U")
+    Parse-tree:
+      None
+
+    Appending chunk 73/103 @ 4:14-4:16 ("RE")
+    Parse-tree:
+      None
+
+    Cutting chunk 73/103 back @ 4:14-4:15 ("R")
+    Parse-tree:
+      None
+
+    Appending chunk 74/103 @ 4:15-4:17 ("E\t")
+    Parse-tree:
+      None
+
+    Cutting chunk 74/103 back @ 4:15-4:16 ("E")
+    Parse-tree:
+      None
+
+    Appending chunk 75/103 @ 4:16-4:18 ("\tD")
+    Parse-tree:
+      None
+
+    Cutting chunk 75/103 back @ 4:16-4:17 ("\t")
+    Parse-tree:
+      None
+
+    Appending chunk 76/103 @ 4:17-4:19 ("DI")
+    Parse-tree:
+      None
+
+    Cutting chunk 76/103 back @ 4:17-4:18 ("D")
+    Parse-tree:
+      None
+
+    Appending chunk 77/103 @ 4:18-4:20 ("IV")
+    Parse-tree:
+      None
+
+    Cutting chunk 77/103 back @ 4:18-4:19 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 78/103 @ 4:19-4:21 ("VI")
+    Parse-tree:
+      None
+
+    Cutting chunk 78/103 back @ 4:19-4:20 ("V")
+    Parse-tree:
+      None
+
+    Appending chunk 79/103 @ 4:20-4:22 ("IS")
+    Parse-tree:
+      None
+
+    Cutting chunk 79/103 back @ 4:20-4:21 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 80/103 @ 4:21-4:23 ("SI")
+    Parse-tree:
+      None
+
+    Cutting chunk 80/103 back @ 4:21-4:22 ("S")
+    Parse-tree:
+      None
+
+    Appending chunk 81/103 @ 4:22-4:24 ("IO")
+    Parse-tree:
+      None
+
+    Cutting chunk 81/103 back @ 4:22-4:23 ("I")
+    Parse-tree:
+      None
+
+    Appending chunk 82/103 @ 4:23-4:25 ("ON")
+    Parse-tree:
+      None
+
+    Cutting chunk 82/103 back @ 4:23-4:24 ("O")
+    Parse-tree:
+      None
+
+    Appending chunk 83/103 @ 4:24-4:26 ("N.")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 83/103 back @ 4:24-4:25 ("N")
+    Parse-tree:
+      None
+
+    Appending chunk 84/103 @ 4:25-5:0 (".\n")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 84/103 back @ 4:25-4:26 (".")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 85/103 @ 4:26-5:1 ("\n ")
+    Parse-tree:
+      None
+
+    Cutting chunk 85/103 back @ 4:26-5:0 ("\n")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 86/103 @ 5:0-5:2 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 86/103 back @ 5:0-5:1 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 87/103 @ 5:1-5:3 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 87/103 back @ 5:1-5:2 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 88/103 @ 5:2-5:4 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 88/103 back @ 5:2-5:3 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 89/103 @ 5:3-5:5 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 89/103 back @ 5:3-5:4 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 90/103 @ 5:4-5:6 ("  ")
+    Parse-tree:
+      None
+
+    Cutting chunk 90/103 back @ 5:4-5:5 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 91/103 @ 5:5-5:7 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 91/103 back @ 5:5-5:6 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 92/103 @ 5:6-5:8 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 92/103 back @ 5:6-5:7 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 93/103 @ 5:7-5:9 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 93/103 back @ 5:7-5:8 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 94/103 @ 5:8-5:10 ("  ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Cutting chunk 94/103 back @ 5:8-5:9 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 95/103 @ 5:9-5:11 (" S")
+    Parse-tree:
+      None
+
+    Cutting chunk 95/103 back @ 5:9-5:10 (" ")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+
+
+    Appending chunk 96/103 @ 5:10-5:12 ("ST")
+    Parse-tree:
+      None
+
+    Cutting chunk 96/103 back @ 5:10-5:11 ("S")
+    Parse-tree:
+      None
+
+    Appending chunk 97/103 @ 5:11-5:13 ("TO")
+    Parse-tree:
+      None
+
+    Cutting chunk 97/103 back @ 5:11-5:12 ("T")
+    Parse-tree:
+      None
+
+    Appending chunk 98/103 @ 5:12-5:14 ("OP")
+    Parse-tree:
+      None
+
+    Cutting chunk 98/103 back @ 5:12-5:13 ("O")
+    Parse-tree:
+      None
+
+    Appending chunk 99/103 @ 5:13-5:15 ("P ")
+    Parse-tree:
+      None
+
+    Cutting chunk 99/103 back @ 5:13-5:14 ("P")
+    Parse-tree:
+      None
+
+    Appending chunk 100/103 @ 5:14-5:16 (" R")
+    Parse-tree:
+      None
+
+    Cutting chunk 100/103 back @ 5:14-5:15 (" ")
+    Parse-tree:
+      None
+
+    Appending chunk 101/103 @ 5:15-5:17 ("RU")
+    Parse-tree:
+      None
+
+    Cutting chunk 101/103 back @ 5:15-5:16 ("R")
+    Parse-tree:
+      None
+
+    Appending chunk 102/103 @ 5:16-5:18 ("UN")
+    Parse-tree:
+      None
+
+    Cutting chunk 102/103 back @ 5:16-5:17 ("U")
+    Parse-tree:
+      None
+
+    Appending chunk 103/103 @ 5:17-5:19 ("N.")
+    Parse-tree:
+      PROGRAM-ID. PROG.
+
+      PROCEDURE DIVISION.
+        STOP RUN. |}];;