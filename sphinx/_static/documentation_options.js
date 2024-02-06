524b79d717151221a5b2ff89340025a5dd580698
engboris <engboris@users.noreply.github.com>
2024-02-06 13:54:48 +0000

    Deploying to gh-pages from  @ 77ed3814068b0eb4c7c4f3850a561f9ea75af0a1 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (642a6ff) (2024/02/06 11:51)',
+    VERSION: 'master (77ed381) (2024/02/06 13:54)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
