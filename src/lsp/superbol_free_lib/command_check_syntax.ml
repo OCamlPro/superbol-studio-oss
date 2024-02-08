94086521971a6cb49dfdc183c084217b5ff3a81a
Boris Eng <boris.eng@ocamlpro.com>
2024-02-08 15:09:20 +0100

    Add check syntax command

---
+++ src/lsp/superbol_free_lib/command_check_syntax.ml
@@ -0,0 +1,51 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (c) 2021-2023 OCamlPro SAS                                  *)
+(*                                                                        *)
+(*  All rights reserved.                                                  *)
+(*  This file is distributed under the terms of the                       *)
+(*  OCAMLPRO-NON-COMMERCIAL license.                                      *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** `parse` command *)
+
+open Ezcmd.V2
+open EZCMD.TYPES
+
+open Common_args
+
+let action { preproc_options; parser_options } files =
+  let parse input =
+    input |>
+    Cobol_preproc.preprocessor ~options:preproc_options |>
+    Cobol_parser.parse_simple ~options:parser_options
+  in
+  List.to_seq files |>
+  Seq.map begin fun filename ->
+    Pretty.out "@[Considering@ `%s'@]@." filename;
+    Cobol_preproc.Input.from ~filename ~f:parse
+  end
+
+let cmd =
+  let files = ref [] in
+    let common, common_args = Common_args.get () in
+
+  EZCMD.sub
+    "parse file"            (* add the corresponding line in Main.subcommands *)
+    begin fun () ->
+      action (common ()) !files |>
+      Seq.iter Cobol_common.Diagnostics.sink_result
+    end
+    ~args:(common_args @ [
+        [],
+        Arg.Anons (fun list -> files := list),
+        EZCMD.info ~docv:"FILE" "Cobol file to parse";
+      ])
+    ~doc: "Parse a Cobol file"
+    ~man:[
+      `S "DESCRIPTION";
+      `Blocks [
+        `P ""
+      ];
+    ]
