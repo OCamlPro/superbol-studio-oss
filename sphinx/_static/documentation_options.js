af537ab6f7285a4119071b5de2899338f2b1f328
lefessan <fabrice.le_fessant@ocamlpro.com>
2024-03-11 09:19:24 +0000

    Deploying to gh-pages from  @ ccebeb28e34f4afc79c586913fc1102a6faf75a9 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (a4fa14c) (2024/03/07 13:26)',
+    VERSION: 'master (ccebeb2) (2024/03/11 09:19)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
