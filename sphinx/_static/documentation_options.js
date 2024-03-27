e161e39a94fbc4d459b94109b492ccee813d68ec
nberth <nberth@member.fsf.org>
2024-03-27 16:32:26 +0000

    Deploying to gh-pages from  @ 26253201fbea246d7e5024ac25f4e070584db2e7 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (d9f7ee4) (2024/03/27 16:10)',
+    VERSION: 'master (2625320) (2024/03/27 16:32)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
