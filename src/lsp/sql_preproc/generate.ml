c2c532f0d379209b066dc1b42efc7186357ffb91
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2024-06-21 16:21:53 +0200

    Add a skeleton for SQL preprocessor

---
+++ src/lsp/sql_preproc/generate.ml
@@ -0,0 +1,161 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (c) 2021-2023 OCamlPro SAS                                  *)
+(*                                                                        *)
+(*  All rights reserved.                                                  *)
+(*  This file is distributed under the terms of the                       *)
+(*  OCAMLPRO-NON-COMMERCIAL license.                                      *)
+(*                                                                        *)
+(**************************************************************************)
+
+open EzCompat
+open Types
+
+let working_storage_section =
+  {|      *> SQL addition in working storage section:
+       01 MY-SQL-STUFF PIC X(9).
+|}
+
+let linkage_section =
+  {|      *> SQL addition in linkage section:
+       01 SOME-ARG PIC X(9).
+|}
+
+let begin_procedure_division ~ctxt:_ ~loc:_ =
+  (* We might want to add something at the begining of PROCEDURE DIVISION ? *)
+  ()
+
+let end_procedure_division ~ctxt:_ ~loc:_ =
+  (* We might want to add something before the end of PROCEDURE DIVISION ? *)
+  ()
+
+let generate ~filename ~contents sql_statements =
+
+  (* split lines and numerotate them *)
+  let lines = EzString.split contents '\n' in
+  let lines = List.mapi (fun i line -> (filename, i+1, line)) lines in
+
+  (* The result will be stored in this buffer: *)
+
+  let b = Buffer.create 1000 in
+
+  let ctxt = { b ;
+               main_filename = filename ;
+             } in
+  let final_loc = { filename; line = -1; char = 0 } in
+
+  let rec output lines statements =
+    match statements with
+    | [] ->
+        List.iter (fun (_,_,line) ->
+            Printf.bprintf ctxt.b "%s\n" line
+          ) lines
+    | (begin_loc, stmt) :: statements ->
+        match begin_loc with
+        | None ->
+            List.iter (fun (_,_,line) ->
+                Printf.bprintf ctxt.b "%s\n" line
+              ) lines ;
+            begin
+              match stmt with
+              | END_PROCEDURE_DIVISION ->
+                  end_procedure_division ~ctxt ~loc:final_loc
+              | _ -> ()
+            end
+        | Some begin_loc ->
+            output_statement lines begin_loc stmt statements
+
+  and output_statement cur_lines begin_loc stmt statements =
+    match cur_lines with
+    | [] -> assert false
+    | (filename,i,line) :: lines ->
+        if filename <> begin_loc.filename || i < begin_loc.line then begin
+          Printf.bprintf ctxt.b "%s\n" line;
+          output_statement lines begin_loc stmt statements
+        end
+        else
+          match stmt with
+          | LINKAGE_SECTION { defined } ->
+              if defined then begin
+                Printf.bprintf ctxt.b "%s\n" line;
+                Buffer.add_string ctxt.b linkage_section;
+                output lines statements
+              end else begin
+                Printf.bprintf ctxt.b "      *> Add missing LINKAGE SECTION\n";
+                Printf.bprintf ctxt.b "       LINKAGE SECTION.\n";
+                Buffer.add_string ctxt.b linkage_section;
+                output cur_lines statements
+              end
+          | WORKING_STORAGE { defined } ->
+              if defined then begin
+                Printf.bprintf ctxt.b "%s\n" line;
+                Buffer.add_string ctxt.b working_storage_section ;
+                output lines statements
+              end else begin
+                Printf.bprintf ctxt.b "      *> Add missing WORKING-STORAGE SECTION\n";
+                Printf.bprintf ctxt.b "       WORKING-STORAGE SECTION.\n";
+                Buffer.add_string ctxt.b working_storage_section;
+                output cur_lines statements
+              end
+          | EXEC_SQL { end_loc ; with_dot ; cmd ; tokens } ->
+              Printf.bprintf ctxt.b "      *> REMOVED: %s\n" line;
+              if i = end_loc.line then begin
+                (* TO BE DONE generate ~loc:begin_loc cmd ~line:i
+                   ~ctxt cmd params *)
+                  ignore (cmd, tokens);
+
+                Misc.add_dot ~with_dot b;
+                output lines statements
+              end else
+                output_statement lines begin_loc
+                  stmt statements;
+          | PROCEDURE_DIVISION_DOT { end_loc } ->
+              Printf.bprintf ctxt.b "      *> REMOVED: %s\n" line;
+              if i = end_loc.line then begin
+                (* for now, just put it back *)
+                Printf.bprintf ctxt.b "          PROCEDURE DIVISION.\n";
+                output lines statements
+              end else
+                output_statement lines begin_loc
+                  stmt statements;
+          | IS_SQLVAR { end_loc } ->
+              if i = begin_loc.line then begin
+                let before_macro = String.sub line 0 begin_loc.char in
+                Printf.bprintf ctxt.b "%s%s" before_macro
+                  "SOME STRING THAT REPLACE IS SQLVAR";
+                if begin_loc.line <> end_loc.line then
+                  Printf.bprintf ctxt.b "\n          ";
+              end;
+              if i = end_loc.line then
+                let len = String.length line in
+                (* This code won't work with tabulations, because
+                   the end_loc.char is wrong in such a case *)
+                let after_macro =
+                  String.sub line (end_loc.char+1) (len-end_loc.char-1) in
+                Printf.bprintf ctxt.b "%s\n" after_macro ;
+                output lines statements
+              else
+                output_statement lines begin_loc stmt statements
+          | BEGIN_PROCEDURE_DIVISION { enabled } ->
+              if !enabled then
+                begin_procedure_division ~ctxt ~loc:begin_loc
+              else
+                Printf.bprintf ctxt.b "      *> BEGIN PROCEDURE DIVISION disabled\n";
+              output cur_lines statements
+          | END_PROCEDURE_DIVISION ->
+              end_procedure_division ~ctxt ~loc:begin_loc;
+              output cur_lines statements
+          | COPY { end_loc ; filename ; contents } ->
+            Printf.bprintf ctxt.b "      *> INLINED: %s\n" line;
+            if i = end_loc.line then begin
+              let copylines = EzString.split contents '\n' in
+              let copylines = List.mapi (fun i line ->
+                  (filename, i+1, line)) copylines in
+              let lines = copylines @ lines in
+              output lines statements
+              end else
+                output_statement lines begin_loc
+                  stmt statements;
+  in
+  output lines sql_statements;
+  Buffer.contents b
---
+++ src/lsp/sql_preproc/types.ml
@@ -0,0 +1,55 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (c) 2021-2023 OCamlPro SAS                                  *)
+(*                                                                        *)
+(*  All rights reserved.                                                  *)
+(*  This file is distributed under the terms of the                       *)
+(*  OCAMLPRO-NON-COMMERCIAL license.                                      *)
+(*                                                                        *)
+(**************************************************************************)
+
+open EzCompat
+
+type error =
+  | Failure of string
+
+exception Error of error
+
+type loc = { filename : string ;
+             line : int ;
+             char : int }
+
+(* These statements show how we could keep information and modify the
+   corresponding places in the code *)
+
+type statements =
+  | PROCEDURE_DIVISION_DOT of { end_loc : loc }
+  | WORKING_STORAGE of { defined: bool }
+  | LINKAGE_SECTION of { defined: bool }
+  | EXEC_SQL of { end_loc : loc ;
+                   with_dot : bool ;
+                   cmd : string ;
+                   tokens : string list ;
+                 }
+  | BEGIN_PROCEDURE_DIVISION of { enabled : bool ref }
+  | END_PROCEDURE_DIVISION
+  | COPY of { end_loc : loc ; filename : string ; contents : string }
+  | IS_SQLVAR of { end_loc : loc }
+
+
+type handle = {
+  mutable handle_abend : string ;
+}
+
+type gen_context = {
+  b : Buffer.t ;
+  main_filename : string ;
+}
+
+type config = {
+  scanner_config: Cobol_indent.Types.config ;
+  sql_in_copybooks : bool;
+  copy_path : ( string * string StringMap.t) list Lazy.t ;
+  copy_exts : string list ;
+  verbosity : int ;
+}
