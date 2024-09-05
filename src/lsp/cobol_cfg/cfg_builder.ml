e07fd9003e13b40b6df88f0d2ec738368cfdeb8f
Mateo <mateo.dugas@ocamlpro.com>
2024-09-05 15:35:54 +0200

    feat: improved arc and add evaluate stmt

--- src/lsp/cobol_cfg/cfg_builder.ml
+++ src/lsp/cobol_cfg/cfg_builder.ml
@@ -50,33 +50,28 @@ end
 type qualname = Cobol_ptree.qualname
 
 type jumps =
-  | Goback
   | Go of qualname
-  | Conditional of qualname (* includes perform, go ... depending *)
+  | GoDepending of qualname
+  | Perform of qualname
 
 module Qualnames = Set.Make(struct
   type t = qualname
   let compare = Cobol_ptree.compare_qualname
 end)
 
-module Jumps = struct
-  include Set.Make(struct
-      type t = jumps
-      let compare j1 j2 =
-        let to_int = function
-          | Goback -> 0
-          | Go _ -> 1
-          | Conditional _ -> 2 in
-        match j1, j2 with
-        | Go qn1, Go qn2 -> Cobol_ptree.compare_qualname qn1 qn2
-        | Conditional qn1, Conditional qn2 -> Cobol_ptree.compare_qualname qn1 qn2
-        | _ -> to_int j2 - to_int j1
-    end)
-  let only_conditional : t -> bool =
-    for_all begin function
-      | Conditional _ -> true | _ -> false
-    end
-end
+module Jumps = Set.Make(struct
+    type t = jumps
+    let compare j1 j2 =
+      let to_int = function
+        | Go _ -> 0
+        | GoDepending _ -> 1
+        | Perform _ -> 2 in
+      match j1, j2 with
+      | Go qn1, Go qn2
+      | GoDepending qn1, GoDepending qn2
+      | Perform qn1, Perform qn2 -> Cobol_ptree.compare_qualname qn1 qn2
+      | _ -> to_int j2 - to_int j1
+  end)
 
 module Qmap = Map.Make(struct
     type t = qualname
@@ -90,6 +85,8 @@ type node = {
   loc: srcloc option;
   entry: bool;
   jumps: Jumps.t;
+  will_fallthru: bool;
+  terminal: bool;
   is_external: bool;
 }
 
@@ -105,55 +102,94 @@ let full_qn' ~cu qn = full_qn ~cu ~&qn
 
 let node_idx = ref 0
 
-let build_node ~default_name ~cu paragraph =
-  let open struct
-    type acc = {
-      jumps: Jumps.t;
-      unreachable: bool;
+let listsplit3 l =
+  List.fold_left begin fun (a_acc, b_acc, c_acc) (a, b, c) ->
+    (a::a_acc, b::b_acc, c::c_acc)
+  end ([], [], []) l
+
+module JumpCollector = struct
+  type acc = {
+    jumps: Jumps.t;
+    will_fallthru: bool;
+    terminal: bool;
+  }
+  let init = { jumps = Jumps.empty;
+               terminal = false;
+               will_fallthru = true; }
+  let add_unconditional uncond acc =
+    {
+      jumps = Jumps.add uncond acc.jumps;
+      terminal = false;
+      will_fallthru = true;
     }
-    let init = { jumps = Jumps.empty;
-                 unreachable = false; }
-    let add_unconditional uncond acc =
-      { jumps = Jumps.add uncond acc.jumps;
-        unreachable = true; }
-    let add_conditionals acc qn_to_jump =
-      { acc with jumps = Jumps.add (Conditional qn_to_jump) acc.jumps }
-  end in
-  let { jumps; unreachable = _ } =
-    Visitor.fold_procedure_paragraph'
-      object (v)
-        inherit [acc] Visitor.folder
-        method! fold_goback' _ acc = skip @@ add_unconditional Goback acc
-        method! fold_statement' _ ({ unreachable; _ } as acc) =
-          if unreachable
-          then skip acc
-          else do_children acc
-        method! fold_if' { payload = { then_branch; else_branch; _ }; _ } acc =
-          let {  jumps; unreachable } =
-            Cobol_ptree.Visitor.fold_statements v then_branch acc in
-          let { jumps = else_j; unreachable = else_unreach } =
-            Cobol_ptree.Visitor.fold_statements v else_branch init in
-          skip {
-            jumps = Jumps.union jumps else_j;
-            unreachable = unreachable && else_unreach
-          }
-        method! fold_goto' { payload; _ } acc =
-          skip @@
-          match payload with
-          | GoToEntry _ -> acc (* TODO couldn't find doc *)
-          | GoToSimple { target } ->
-            add_unconditional (Go (full_qn' ~cu target)) acc
-          | GoToDepending { targets; _ } ->
-            Cobol_common.Basics.NEL.to_list targets
-            |> List.map (full_qn' ~cu)
-            |> List.fold_left add_conditionals acc
-        method! fold_perform_target' { payload; _ } acc =
-          skip @@
-          let { payload = start; _ } = payload.perform_target.procedure_start in
-          add_conditionals acc (full_qn ~cu start)
-      end
-      paragraph init
-  in
+  let folder ~cu = object (v)
+    inherit [acc] Visitor.folder
+    method! fold_goback' _ acc = skip @@ { acc with terminal = true; will_fallthru = false }
+    method! fold_stop' _ acc = skip @@ { acc with terminal = true; will_fallthru = false }
+    method! fold_exit' { payload = exit_stmt; _ } acc =
+      skip @@
+      match exit_stmt with
+      | ExitSimple | ExitPerform _
+      | ExitMethod _ | ExitProgram _ | ExitFunction _ -> acc
+      | ExitParagraph -> { acc with will_fallthru = true }
+      | ExitSection -> { acc with will_fallthru = true } (* TODO: go to next section ? *)
+    method! fold_evaluate' { payload; _ } acc =
+      let { eval_branches; eval_otherwise; _ }: Cobol_ptree.evaluate_stmt =
+        payload in
+      let jumps, terminals, unreachables = List.map begin fun branch ->
+          let { jumps; terminal; will_fallthru } =
+            Cobol_ptree.Visitor.fold_evaluate_branch v branch init in
+          (jumps, terminal, will_fallthru)
+        end eval_branches |> listsplit3 in
+      let other =
+        Cobol_ptree.Visitor.fold_statements v eval_otherwise init in
+      skip {
+        jumps = List.fold_left Jumps.union acc.jumps (other.jumps::jumps);
+        will_fallthru = List.fold_left (||) other.will_fallthru unreachables;
+        terminal = List.fold_left (||) other.terminal terminals;
+      }
+    method! fold_statement' _ ({ will_fallthru; _ } as acc) =
+      if will_fallthru then do_children acc else skip acc
+    method! fold_if' { payload = { then_branch; else_branch; _ }; _ } acc =
+      let {  jumps; terminal; will_fallthru } =
+        Cobol_ptree.Visitor.fold_statements v then_branch acc in
+      let { jumps = else_jumps;
+            terminal = else_terminal;
+            will_fallthru = else_fallthru } =
+        Cobol_ptree.Visitor.fold_statements v else_branch init in
+      skip {
+        jumps = Jumps.union jumps else_jumps;
+        will_fallthru = will_fallthru || else_fallthru;
+        terminal = terminal || else_terminal;
+      }
+    method! fold_goto' { payload; _ } acc =
+      skip @@
+      match payload with
+      | GoToEntry _ -> acc (* TODO couldn't find doc *)
+      | GoToSimple { target } ->
+        {
+          acc with
+          jumps = Jumps.add (Go (full_qn' ~cu target)) acc.jumps;
+          will_fallthru = false;
+        }
+      | GoToDepending { targets; _ } ->
+        Cobol_common.Basics.NEL.(
+          targets
+          |> map ~f:(full_qn' ~cu)
+          |> fold_left ~f:begin fun acc target ->
+            Jumps.add (GoDepending target) acc
+          end acc.jumps)
+        |> begin fun jumps -> { acc with jumps } end
+    method! fold_perform_target' { payload; _ } acc =
+      let start = full_qn' ~cu payload.perform_target.procedure_start in
+      skip { acc with jumps = Jumps.add (Perform start) acc.jumps }
+  end
+end
+
+let build_node ~default_name ~cu paragraph =
+  let open JumpCollector in
+  let { jumps; will_fallthru; terminal; } =
+    Visitor.fold_procedure_paragraph' (folder ~cu) paragraph init in
   node_idx:=!node_idx+1;
   let qid, loc = match ~&paragraph.paragraph_name with
     | None -> default_name, ~@paragraph
@@ -166,6 +202,8 @@ let build_node ~default_name ~cu paragraph =
     loc = Some loc;
     entry = false;
     jumps;
+    will_fallthru;
+    terminal;
     is_external = false;
   }
 
@@ -181,8 +219,8 @@ end
 
 type edge =
   | FallThrough
-  | Conditional
-  | Unconditional
+  | Perform
+  | Go
 
 module Edge = struct
    type t = edge
@@ -191,8 +229,8 @@ module Edge = struct
    let default = FallThrough
    let to_string = function
      | FallThrough -> "f"
-     | Conditional -> "c"
-     | Unconditional -> "u"
+     | Perform -> "c"
+     | Go -> "u"
 end
 
 module Cfg = Graph.Persistent.Digraph.ConcreteLabeled(Node)(Edge)
@@ -217,8 +255,8 @@ module Dot = Graph.Graphviz.Dot(struct
     let edge_attributes (_,s,_) =
       [`Style (match s with
            | FallThrough -> `Dotted
-           | Conditional -> `Dashed
-           | Unconditional -> `Solid)]
+           | Perform -> `Dashed
+           | Go -> `Solid)]
     let default_edge_attributes _ = []
     let get_subgraph _ = None
     let vertex_attributes ({ entry; is_external; _ } as n) =
@@ -245,6 +283,8 @@ let dummy_node qn =
     entry = false;
     names = NEL.One (qn_to_string qn);
     jumps = Jumps.empty;
+    will_fallthru = false;
+    terminal = false;
     is_external = true;
   }
 
@@ -264,21 +304,21 @@ let rec build_edges ~vertexes g nodes =
     | ({ jumps; _ } as current)::_ ->
       Jumps.fold begin fun uncond (g, vertexes) ->
         match uncond with
-        | Goback -> g, vertexes
+        | GoDepending jump_to
         | Go jump_to ->
           let vertexes, next = qmap_find_or_add vertexes jump_to in
-          Cfg.add_edge_e g (current, Unconditional, next),
+          Cfg.add_edge_e g (current, Go, next),
           vertexes
-        | Conditional jump_to ->
+        | Perform jump_to ->
           let vertexes, next = qmap_find_or_add vertexes jump_to in
-          Cfg.add_edge_e g (current, Conditional, next),
+          Cfg.add_edge_e g (current, Perform, next),
           vertexes
       end jumps (g, vertexes)
     | [] -> g, vertexes
   in
   match nodes with
-  | ({ jumps; _ } as current)::next::tl
-    when Jumps.only_conditional jumps ->
+  | ({ will_fallthru; _ } as current)::next::tl
+    when will_fallthru ->
     build_edges ~vertexes (Cfg.add_edge g current next) (next::tl)
   | _::tl -> build_edges ~vertexes g tl
   | [] -> g
