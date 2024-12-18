5f6d97e418cdafb283af1d8a02b75c2bd3c0b61e
emilienlemaire <emilien.lem@icloud.com>
2024-12-18 12:21:15 +0000

    Deploying to gh-pages from  @ f1dc62e277e62524c906f9a2920a2e64242a746f 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (857dc31) (2024/12/16 11:04)',
+    VERSION: 'master (f1dc62e) (2024/12/18 12:21)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
