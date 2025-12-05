0a14099f4d2b972be02c1753fd0355b98b79cf8a
Emilien Lemaire <emilien.lem@icloud.com>
2025-12-05 14:30:08 +0100

    Update schema

--- src/lsp/superbol_free_lib/vscode_extension.ml
+++ src/lsp/superbol_free_lib/vscode_extension.ml
@@ -596,7 +596,7 @@ let contributes =
       Manifest.tomlValidation
         ~fileMatch:"superbol.toml"
         (* TODO: change this address to a more permanent one; also, substitute `master` for a version tag *)
-        ~url:"https://raw.githubusercontent.com/OCamlPro/superbol-studio-oss/master/schemas/superbol-schema-0.1.4.json";
+        ~url:"https://raw.githubusercontent.com/OCamlPro/superbol-studio-oss/master/schemas/superbol-schema-0.2.3.json";
     ]
     ~menus: [
       "editor/context",
