b7dbc01eee44f4bc121e6b2d3a1284df93e29e62
Emilien Lemaire <emilien.lemaire@ocamlpro.com>
2026-02-11 15:15:19 +0100

    Use simple string in inspect_where printer

--- src/lsp/cobol_ptree/simple_statements.ml
+++ src/lsp/cobol_ptree/simple_statements.ml
@@ -404,10 +404,7 @@ let pp_inspect_direction ppf = function
 let pp_inspect_where =
   Fmt.(pair
     ~sep:sp pp_inspect_direction
-      Pretty.(NEL.pp ~fopen:(Simple.string " ")
-                     ~fsep:(Pretty.Simple.string "@ OR@ ")
-                     ~fclose:(Simple.string " ")
-                      pp_ident_or_nonnum))
+      (NEL.pp ~fopen:"@ " ~fsep:"@ OR@ "~fclose:"@ " pp_ident_or_nonnum))
 
 let pp_converting ppf {
   converting_from = cf; converting_to = ct; converting_where = cw
