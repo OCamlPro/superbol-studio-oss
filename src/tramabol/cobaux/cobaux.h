490234bec8cdbadd28b826e7f20043c9134c5b77
Nicolas Berthier <nicolas.berthier@ocamlpro.com>
2026-04-29 11:22:51 +0200

    Remove some code in `cobaux` that's unused in `ctypes`-based bindings

--- src/tramabol/cobaux/cobaux.h
+++ src/tramabol/cobaux/cobaux.h
@@ -16,11 +16,6 @@
 
 #include "libcob.h"
 
-typedef struct __memory {
-  size_t size;
-  unsigned char *array;
-} memory_t;
-
 /* cobaux_module.c */
 
 extern int
@@ -31,24 +26,5 @@ cobaux_module_init (cob_module **module_ptr,
 
 extern void
 cobaux_module_enter (cob_module * const module);
-
-/* cobaux_field.c */
-
-/* struct __cob_field * */
-/* cobaux_field_create (struct __memory * const memory, */
-/* 		     const size_t offset, */
-/* 		     const size_t size, */
-/* 		     const struct __cob_field_attr * const attrs); */
-
-/* cobaux_termio.c */
-
-/* Note: unused for now */
-extern void
-cobaux_display (const int to_device, const int newline, const int varcnt,
-		cob_field ** fields);
-
-/* Note: unused for now */
-extern void
-cobaux_display1 (const int to_device, const int newline, cob_field * field);
 
 #endif	/* __COBAUX_H__ */
