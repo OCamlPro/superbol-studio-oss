f70891c8afe1697f73fe63fabf6892200fa1574e
David Declerck <david.declerck@ocamlpro.com>
2026-06-16 11:21:15 +0200

    Add JS bindings scope to avoid clash with other extensions

--- src/vendor/vscode-ocaml-platform/src-bindings/vscode_languageclient/vscode_languageclient_stub.js
+++ src/vendor/vscode-ocaml-platform/src-bindings/vscode_languageclient/vscode_languageclient_stub.js
@@ -1,1 +1,6 @@
-globalThis.vscode_languageclient = require("vscode-languageclient");
+
+if (globalThis.__SUPERBOL__ === undefined) {
+  globalThis.__SUPERBOL__ = new Object();
+}
+
+globalThis.__SUPERBOL__.vscode_languageclient = require("vscode-languageclient");
