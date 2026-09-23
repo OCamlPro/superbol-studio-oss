56348110b6abb7e2588716992a1ff6770b7b720b
GBertholon <guillaume.bertholon@ocamlpro.com>
2026-09-23 16:49:50 +0000

    Deploying to gh-pages from  @ 82b23b844250030b1a35d9bb93828b25bf1246f1 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'master (d91530a) (2026/09/23 15:23)',
+    VERSION: 'master (82b23b8) (2026/09/23 16:49)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
